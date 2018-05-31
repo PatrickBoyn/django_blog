384102bfdfe781f1a99f2e45dd48bb6fcb79dcbc
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-05-31 17:10:01 -0400

    The main fields of the Comment class are done.

--- mysite/models.py
+++ mysite/models.py
@@ -19,3 +19,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey('mysite.Post', on_delete=models.CASCADE)
+    author = models.CharField(max_length=100)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    approved_comment = models.BooleanField(default=False)
+    
+
+
